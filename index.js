7a6a8036637d73a66a581ff5bf8193eee65cd75f
ying1488 <yingecteach@gmail.com>
2024-07-02 17:29:28 +1000

    .

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const bcrypt = require('bcrypt');
 
 
 const hashPassword = async (pw)=>{
-const salt = await bcrypt.genSalt(12);
-const hash = await bcrypt.hash(pw,salt);
-console.log(salt);
+
+const hash = await bcrypt.hash(pw,12);
+
 console.log(hash);
 }
 
@@ -18,4 +18,4 @@ const login = async(pw,hashedPw)=>{
 }
 
 // hashPassword('monkey');
-login('monkey','$2b$12$QspSKj/TlDjfIx/mgPYAgOKQNeehCcDxsnkE7cms2jrn80cBTGxcq')
+login('monkey122','$2b$12$QspSKj/TlDjfIx/mgPYAgOKQNeehCcDxsnkE7cms2jrn80cBTGxcq')
